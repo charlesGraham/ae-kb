a5b9a2dcb450b6b17ed6f249a547ad5f28fc05a1
Charles <crg0905@gmail.com>
2021-10-14 13:28:29 -0700

    catch-all

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Footer from './Footer';
 import Header from './Header';
 
 function App() {
+
+  /* initialize state to some home screen */
+
+
   return (
     <div className="App">
 
       <Header />
 
+      { /* wrap body in a router to navigate between pages */ }
       <Body />
 
       <Footer />
